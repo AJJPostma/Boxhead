e75aeb24189855ddf236dc792f635ce915ffe667
BuildTools <ducod16@gmail.com>
2016-11-05 14:29:08 +0100

    php shit added

--- dataconnect.php
+++ dataconnect.php
@@ -7,6 +7,12 @@
 <body>
 
 <?php
+$q = $_REQUEST["q"];
+
+$n = $_REQUEST["n"];
+//$n = mysql_real_escape_string($n);
+
+echo "{$n} en {$q}" ;
 $servername = "eu-cdbr-azure-west-d.cloudapp.net:3306";
 $username = "b854fea3f26f48";
 $password = "3fd6ee20";
@@ -21,17 +27,22 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
-$conn->close();
+$sql = "INSERT INTO score (name, score)
+VALUES ('{$n}', $q)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+
+
+
+
 ?>
 
-<form action="action_page.php">
-    name:<br>
-    <input type="text" name="htmlname" value="">
-    <br>
-    score:<br>
-    <input type="text" name="htmlscore" value="">
-    <br><br>
-    <input type="submit" value="Submit">
-</form>
+
 </body>
 </html>
